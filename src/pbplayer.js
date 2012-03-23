2c609cece56e61d4867f5d78b511afa434a9dbad
Niek Saarberg <n.saarberg@gmail.com>
2012-03-23 15:12:29 +0100

    Rewrite

---
+++ src/pbplayer.js
@@ -0,0 +1,33 @@
+//= compat
+/*!
+ * pbPlayer v<%= PB_VERSION %>
+ * https://github.com/Pluxbox/pbPlayer
+ *
+ * Requires pbjs javascript framework (> 0.5)
+ * https://github.com/Saartje87/pbjs
+ */
+(function ( context, undefined ){
+
+"use strict";
+	
+if( context.PB === undefined ) {
+	
+	throw new Error('pbjs required. Visit https://github.com/Saartje87/pbjs for the latest release!');
+}
+
+// PBPlayer should only be declared once
+if( context.PB.Player !== undefined ) {
+	
+	return;
+}
+
+// Speed up reference
+var PB = context.PB,
+	instances = [];
+
+//= require "./core/core"
+
+//= require "./containers/containers"
+
+})( this );
+
