7e33b0e8ef0e7897e8f362ffab78e99b73ce2001
Niek Saarberg <n.saarberg@gmail.com>
2013-07-10 14:54:05 +0200

    Added playlist.js to grunt

--- dist/pbplayer.js
+++ dist/pbplayer.js
@@ -8,7 +8,7 @@
  * Copyright 2013 Pluxbox
  * Licensed MIT
  *
- * Build date 2013-07-10 14:26
+ * Build date 2013-07-10 14:27
  */
 (function ( name, context, definition ) {
 	
@@ -93,5 +93,84 @@ pbPlayer.config = function ( config ) {
 	PB.overwrite(pbPlayer.defaults, config);
 }; // Set defaults for all pbplayer instances
 
+var Playlist = PB.Class(PB.Observer, {
+
+	construct: function() {
+
+		this.parent();
+
+		this._entries = [];
+		this._currentEntryIndex = 0;
+	},
+
+	/**
+	 * Adds a media object to the playlist.
+	 * @param {Object} The media object to add to the playlist.
+	 */
+	add: function( media ) {
+
+		if( typeof media !== 'object' ) {
+			return;
+		}
+
+		this._entries.push(media);
+		this.emit('mediaadded', { media: media });
+	},
+
+	/**
+	 * Removes a media object to the playlist.
+	 * @param {Object} The media object to remove from the playlist.
+	 */
+	remove: function( media ) {
+
+		var index = this._entries.indexOf(media);
+
+		if( index !== -1 ) {
+			this.emit('mediaremoved', { media: this._entries.splice(index, 1)[0] });
+		}
+	},
+
+	/**
+	 * Removes all media objects from the playlist.
+	 */
+	empty: function() {
+		
+		while( this._entries.length ) {
+
+			this.remove(this._entries[0]);
+		}
+	},
+
+	/**
+	 * Switches to the next media object in the playlist, if any.
+	 */
+	next: function() {
+
+		var entry = this._entries[this._currentEntryIndex + 1];
+
+		if( entry === undefined ) {
+			return;
+		}
+
+		this._currentEntryIndex++;
+		this.emit('mediachanged', { media: entry });
+	},
+
+	/**
+	 * Switches to the previous media object in the playlist, if any.
+	 */
+	previous: function() {
+
+		var entry = this._entries[this._currentEntryIndex - 1];
+
+		if( entry === undefined ) {
+			return;
+		}
+
+		this._currentEntryIndex--;
+		this.emit('mediachanged', { media: entry });
+	}
+
+});
 return pbPlayer;
 });
