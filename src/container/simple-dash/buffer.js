c57b98ed80e0a9b34b361a7f7f96bd80cf8f79f5
Jon Koops <jonkoops@gmail.com>
2014-01-24 11:38:24 +0100

    Updated buffer class

--- src/container/simple-dash/buffer.js
+++ src/container/simple-dash/buffer.js
@@ -4,19 +4,42 @@
 
 		this._manifest = manifest;
 		this._chunks = [];
+		this._bufferedChunks = 0;
+		this._currentChunk = 0;
 		this._minChunks = 4;
 		this._maxChunks = 20;
 	};
 
-	Buffer.prototype.startBuffering = function() {
+	Buffer.prototype.start = function() {
 
 		this._bufferChunk();
+	};
+
+	Buffer.prototype.getChunk = function() {
+
+		var chunk = this._chunks[this._currentChunk];
+
+		// TODO: Check for possible non-buffered chunk
+
+		this._bufferedChunks--;
+		this._currentChunk++;
+
+		this._bufferChunk();
+
+		return chunk;
 	};
 
 	Buffer.prototype._bufferChunk = function() {
 
 		if( !this._manifest.hasChunk() ) {
-			throw 'The manifest has no chunks.';
+			throw 'The manifest has no chunks left.';
+		}
+
+		// Check if we need a new chunk
+		if( this._bufferedChunks > this._minChunks &&
+			this._bufferedChunks > this._maxChunks ) {
+
+			return;
 		}
 
 		this._manifest.getChunk().then(function( chunk ) {
@@ -28,12 +51,9 @@
 			console.log(chunk);
 
 			this._chunks.push(chunk);
+			this._bufferedChunks++;
 
-			if( this._chunks.length < this._minChunks &&
-				this._chunks.length < this._maxChunks ) {
-
-				this._bufferChunk();
-			}
+			this._bufferChunk();
 
 		}.bind(this));
 	};
