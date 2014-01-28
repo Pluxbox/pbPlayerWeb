196bf41f9d5d78859e7250969a28c1c6e0601812
Jon Koops <jon@localhost.localdomain>
2014-01-28 16:58:48 +0100

    Updated the ManifestReader to hold it's segements in memory + added annotations

--- src/container/simple-dash/manifest-reader.js
+++ src/container/simple-dash/manifest-reader.js
@@ -7,40 +7,76 @@ var SimpleDash = SimpleDash || {};
 	var ManifestReader = function( src ) {
 
 		this._src = src;
-		this._manifest = null;
+		this._manifestLoaded = false;
 		this._segments = [];
+		this._currentSegment = 0;
 	};
 
-	ManifestReader.prototype._requestManifest = function() {
+	/**
+	 * Loads the manifest from the server and parses it.
+	 *
+	 * @param {String} src The url where the manifest can be found.
+	 * @returns {Promise} A promise for when loading succeeds or fails.
+	 */
+	ManifestReader.prototype._loadManifest = function( src ) {
 
 		return new Promise(function( resolve, reject ) {
 
 			var request = new XMLHttpRequest();
-			request.open('GET', this._src, true);
+			request.open('GET', src, true);
 
 			request.onload = function() {
 
-				this._manifest = JSON.parse(request.response);
-				this._addSegments(this._manifest.containers[0].segments);
-				resolve();
+				try {
+
+					var manifest = JSON.parse(request.response);
+
+					// TODO: Add support for selecting a container
+					this._appendSegments(manifest.containers[0].segments);
+					this._manifestLoaded = true;
+
+					resolve();
+
+				} catch( err ) {
+
+					reject('Could not parse manifest.')
+				}
+
 
 			}.bind(this);
 
-			request.onerror = reject;
+			request.onerror = function() {
+
+				reject('Could not load manifest from server.');
+			};
 
 			request.send();
 
 		}.bind(this));
 	};
 
-	ManifestReader.prototype._addSegments = function( segments ) {
+	/**
+	 * Appends a bunch of segments removing duplicates in the proccess.
+	 *
+	 * @param {Array} segments The segments to append.
+	 */
+	ManifestReader.prototype._appendSegments = function( segments ) {
 
+		// Get current segment ids
 		var currentSegments = this._getSegmentIds(this._segments);
 
-		// Filter out all duplicate ckunks
+		// Filter out all unwanted segments
 		segments = segments.filter(function( segment ) {
 
-			return segment.type === 'chunk' ? currentSegments.indexOf(segment.id) === -1 : true;
+			if( segment.type === 'chunk' ) {
+				return currentSegments.indexOf(segment.id) === -1;
+			}
+
+			if( segment.type === 'manifest' ) {
+				return true;
+			}
+
+			return false;
 		});
 
 		// Create instances for segments
@@ -58,25 +94,44 @@ var SimpleDash = SimpleDash || {};
 			return segment;
 		});
 
-		return this._segments = this._segments.concat(segments);
+		this._segments = this._segments.concat(segments);
 	};
 
+	/**
+	 * Creates an array of ids from the specified segments.
+	 * 
+	 * @param {Array} segments The segments to extract the ids from.
+	 * @returns {Array} A collection of ids.
+	 */
 	ManifestReader.prototype._getSegmentIds = function( segments ) {
 
-		return segments.map(function( segment ) {
+		var results;
+
+		// Fill results with ids of segments
+		results = segments.map(function( segment ) {
 			return segment.id;
-		}).filter(function( id ) {
+		});
+
+		// Filter out any undefined segments
+		results = results.filter(function( id ) {
 			return id !== undefined;
 		});
+
+		return results;
 	};
 
+	/**
+	 * Checks if there is a segment available.
+	 *
+	 * @returns {Boolean} True if a segment is available, false otherwise.
+	 */
 	ManifestReader.prototype.hasChunk = function() {
 
-		var segment = this._segments[0];
+		var segment = this._segments[this._currentSegment];
 
-		if( this._manifest === null ||
+		if( !this._manifestLoaded ||
 			segment instanceof Chunk ||
-			(segment instanceof ManifestReader && segment.hasChunk()) ) {
+			( segment instanceof ManifestReader && segment.hasChunk() ) ) {
 
 			return true;
 		}
@@ -84,35 +139,43 @@ var SimpleDash = SimpleDash || {};
 		return false;
 	};
 
+	/**
+	 * Get the next chunk in the manifest in sequence.
+	 *
+	 * @returns {Promise} A promise that resolves with the Chunk.
+	 */
 	ManifestReader.prototype.getChunk = function() {
 
-		// Retrieve manifest from server if it's not set
-		if( this._manifest === null ) {
-			return this._requestManifest().then(this.getChunk.bind(this));
+		// Load manifest if not yet loaded
+		if( !this._manifestLoaded ) {
+
+			return this._loadManifest(this._src)
+				.then(this.getChunk.bind(this));
 		}
 
-		var segment = this._segments[0];
+		// Get current segment
+		var segment = this._segments[this._currentSegment];
 
+		// Resolve if segment is of type chunk
 		if( segment instanceof Chunk ) {
-			this._segments.shift();
+
+			this._currentSegment++;
 			return Promise.resolve(segment);
 		}
 
+		// Get chunk from nested manifestreader or load next chunk if it's out of chunks
 		if( segment instanceof ManifestReader ) {
 
 			if( segment.hasChunk() ) {
 				return segment.getChunk();
 			} else {
-				this._segments.shift();
-				return this.getChunk();
+				this._currentSegment++;
+				return this.getChunk()
 			}
 		}
 
-		if( segment === undefined ) {
-			return Promise.reject('There are no more chunks in the manifest.');
-		}
-
-		return Promise.resolve(segment);
+		// There are no more comparisons, segment is unknown
+		return Promise.reject('Got an unknown segment on index ' + (this._currentSegment + 1));
 	};
 
 	SimpleDash.ManifestReader = ManifestReader;
